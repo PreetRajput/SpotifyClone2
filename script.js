1b4a0923abdb79e8dc8a309caab58e6b3842516a
Ajay <sgoku9986@gmail.com>
2023-09-09 21:20:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('hello js');
+
+let content= document.querySelectorAll('.playBtn');
+content.addEventListener('click',function(){
+    content.style.background ='red';
+});
+
+console.log('hello js');
